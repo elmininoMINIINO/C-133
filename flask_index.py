d4f0501aa5653ab31bc09b4258c56d5863480555
elmininoMINIINO <116765386+elmininoMINIINO@users.noreply.github.com>
2024-03-29 18:35:36 -0500

    Add files via upload

---
+++ flask_index.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+#En la función return render_template('index.html')
+@app.route("/index")
+def first_webpage():
+    #Crear una variable
+    name = 'Flask'
+    #Pasar la variable en la plantilla
+    return render_template('index.html', index_variable = name)
+app.run(debug=True)
